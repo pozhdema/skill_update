f0fa463f6f89d06145c609b1a7ba3f379ca5d24e
Pozhdema <pozhdema107@gmail.com>
2020-03-05 15:22:52 +0200

    added last section and footer

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ const sliderData = [
     },
     {
         button: 'SEE FULL PROJECT',
-        h3: 'SUBSENCE MONOCHROME',
-        p: 'Web design in monochromatic colors for SUBSENCE agency.',
-        i: 'web design',
-        img: 'images/1-hover.png'
+        h3: 'DESIGN AGENCY BUSINESS CARD',
+        p: 'Logotype and business card design for ARTDESIGN agency.',
+        i: 'branding',
+        img: 'images/3-hover.png'
     },
     {
         button: 'SEE FULL PROJECT',
-        h3: 'SUBSENCE MONOCHROME',
-        p: 'Web design in monochromatic colors for SUBSENCE agency.',
-        i: 'web design',
-        img: 'images/1-hover.png'
+        h3: 'FIXEDGEAR SHOP PHOTOSHOOT',
+        p: 'Photoshoot for the annual issue of the fixed gear and road bikes magazine.',
+        i: 'photography',
+        img: 'images/2-hover.png'
     },
     {
         button: 'SEE FULL PROJECT',
-        h3: 'SUBSENCE MONOCHROME',
-        p: 'Web design in monochromatic colors for SUBSENCE agency.',
+        h3: 'EDITOR\'S PERSONAL PAGE',
+        p: 'Web page created for Ryan Fields a famous US editor.',
         i: 'web design',
-        img: 'images/1-hover.png'
+        img: 'images/4-hover.png'
     },
 ]
 
